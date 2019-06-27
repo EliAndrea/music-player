f8431b4bde2f9233cb79a00a274c1b92b2ad1e09
Eli Quijada <eliana.quijada.g@gmail.com>
2019-06-27 01:50:32 -0400

    traducción de código

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import {ListaMusica} from "./components/ListaMusica.js";
-import {Controles} from "./components/Controles.js";
-
-let canciones = [];
+import {MusicList} from "./components/MusicList.js";
+import {Controls} from "./components/Controls.js";
 
 class App extends Component {
   constructor(){
     super();
+    this.song = React.createRef();
     this.state = {
-      lista: canciones
+      list: [],
+      song: "",
     };
-    this.asignarSeleccion = this.asignarSeleccion.bind(this);
-    this.adelantarCancion = this.adelantarCancion.bind(this);
-    this.retrocederCancion = this.retrocederCancion.bind(this);
+    this.updateSongs = this.updateSongs.bind(this);
+    this.forwardSong = this.forwardSong.bind(this);
+    this.backwardSong = this.backwardSong.bind(this);
   }
   
   componentDidMount(){
@@ -22,54 +22,54 @@ class App extends Component {
         return response.json();
       })
       .then((responseAsJson) =>{
-        canciones = responseAsJson;
-        console.log(canciones);
-        this.setState({lista: canciones});
+        let songs = responseAsJson;
+        this.setState({list: songs});
       })
       .catch((error) => {
         alert(error);
       });
   }
 
-  adelantarCancion(arr){
+  forwardSong(arr){
     for(let i=0; i<arr.length; i++){
-      if(arr[i].estado === "seleccionada" && i < arr.length - 1){
-        arr[i].estado = "";
-        arr[i +1].estado = "seleccionada";
+      if(arr[i].selected === "selected" && i < arr.length - 1){
+        arr[i].selected = "";
+        arr[i +1].selected = "selected";
         break;
       }
-      else if(arr[i].estado === "seleccionada" && i === arr.length-1){
-        arr[i].estado = "";
-        arr[0].estado = "seleccionada";
+      else if(arr[i].selected === "selected" && i === arr.length-1){
+        arr[i].selected = "";
+        arr[0].selected = "selected";
         break;
       }
     }
-    this.setState({lista: arr});
+    this.setState({list: arr});
   }
-  retrocederCancion(arr){
+  backwardSong(arr){
     for(let i=arr.length-1; i >= 0; i--){
-      if(arr[i].estado === "seleccionada" && i > 0){
-        arr[i].estado = "";
-        arr[i - 1].estado = "seleccionada";
+      if(arr[i].selected === "selected" && i > 0){
+        arr[i].selected = "";
+        arr[i - 1].selected = "selected";
         break;
       }
-      else if(arr[i].estado === "seleccionada" && i === 0){
-        arr[i].estado = "";
-        arr[arr.length-1].estado = "seleccionada";
+      else if(arr[i].selected === "selected" && i === 0){
+        arr[i].selected = "";
+        arr[arr.length-1].selected = "selected";
         break;
       }
     }
-    this.setState({lista: arr});
+    this.setState({list: arr});
   }
   
-  asignarSeleccion(arr){
-    this.setState({lista: arr});
+  updateSongs(arr){
+    this.setState({list: arr});
   }
   render() {
     return (
       <div className="App">
-        <ListaMusica canciones={canciones} asignar={this.asignarSeleccion}/>
-        <Controles adelantar={this.adelantarCancion} retroceder={this.retrocederCancion} lista={this.state.lista}/>
+        <audio></audio>
+        <MusicList songs={this.state.list} update={this.updateSongs}/>
+        <Controls forward={this.forwardSong} backward={this.backwardSong} list={this.state.list}/>
       </div>
     );
   }
